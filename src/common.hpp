de91717ee6d2d3b91151d1fad92889c4a4bb4e6a
Your Name <you@example.com>
2023-09-01 16:24:28 +0800

    wbserver

--- src/common.hpp
+++ src/common.hpp
@@ -3,6 +3,40 @@
 #include "log_helper.hpp"
 #include "use_header.hpp"
 
+enum guac_rdp_security
+{
+	/**
+	 * Legacy RDP encryption.
+	 */
+	GUAC_SECURITY_RDP,
+
+	/**
+	 * TLS encryption.
+	 */
+	GUAC_SECURITY_TLS,
+
+	/**
+	 * Network level authentication.
+	 */
+	GUAC_SECURITY_NLA,
+
+	/**
+	 * Extended network level authentication.
+	 */
+	GUAC_SECURITY_EXTENDED_NLA,
+
+	/**
+	 * Negotiate security methods supported by Hyper-V's "VMConnect" feature.
+	 */
+	GUAC_SECURITY_VMCONNECT,
+
+	/**
+	 * Negotiate a security method supported by both server and client.
+	 */
+	GUAC_SECURITY_ANY
+
+};
+
 struct guac_rdp_bitmap
 {
 	/**
@@ -26,15 +60,15 @@ static BOOL guac_rdp_bitmap_new(rdpContext* context, rdpBitmap* bitmap)
 {
 	// TODO: Implement custom bitmap data structure creation and initialization
 	// ...
-	LOG_INFO(__func__);
+	// LOG_INFO(__func__);
 	return true;
 }
 
 static void guac_rdp_bitmap_free(rdpContext* context, rdpBitmap* bitmap)
 {
-	LOG_INFO(__func__);
-	// TODO: Implement custom bitmap data structure release
-	// ...
+	// LOG_INFO(__func__);
+	//  TODO: Implement custom bitmap data structure release
+	//  ...
 }
 
 static BOOL guac_rdp_bitmap_paint(rdpContext* context, rdpBitmap* bitmap)
@@ -126,9 +160,9 @@ static BOOL guac_rdp_glyph_enddraw(rdpContext* context,
 
 static BOOL guac_rdp_pointer_new(rdpContext* context, rdpPointer* pointer)
 {
-	LOG_INFO(__func__);
-	// TODO: Implement custom pointer data structure creation and initialization
-	// ...
+	// LOG_INFO(__func__);
+	//  TODO: Implement custom pointer data structure creation and initialization
+	//  ...
 
 	return true;
 }
@@ -176,4 +210,39 @@ static BOOL guac_rdp_keyboard_set_indicators(rdpContext* context, UINT16 flags)
 	LOG_INFO(__func__);
 	// Initializatio
 	return TRUE;
+}
+
+struct ImageData
+{
+	int					  x;		// x坐标
+	int					  y;		// y坐标
+	int					  width;	// 图像宽度
+	int					  height;	// 图像高度
+	std::vector<png_byte> pngData;	// PNG图像数据
+};
+
+struct MouseEvent
+{
+	int x;
+	int y;
+	int mask;
+};
+
+inline std::string serialize(const ImageData& data)
+{
+	std::string result;
+	// 添加坐标和尺寸信息
+	result.append(reinterpret_cast<const char*>(&data.x), sizeof(data.x));
+	result.append(reinterpret_cast<const char*>(&data.y), sizeof(data.y));
+	result.append(reinterpret_cast<const char*>(&data.width), sizeof(data.width));
+	result.append(reinterpret_cast<const char*>(&data.height), sizeof(data.height));
+
+	// 添加PNG数据长度
+	size_t pngSize = data.pngData.size();
+	result.append(reinterpret_cast<const char*>(&pngSize), sizeof(pngSize));
+
+	// 添加PNG数据
+	result.append(reinterpret_cast<const char*>(data.pngData.data()), pngSize);
+
+	return result;
 }
